0dbc04ca1bf0c76c3b94fdface13c38af1405858
Владислав Драгушин <maxhc@inbox.ru>
2026-09-21 18:34:59 +0000

    Added new directory simple_app

---
+++ simple_app/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+N=5
+LOG_FILE="monitor.log"
+
+while true; do
+    TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+    echo "--- $TIMESTAMP ---" >> "$LOG_FILE"
+    echo "--- Состояние оперативной памяти ---" >> "$LOG_FILE"
+    free -h >> "$LOG_FILE"
+    echo "--- Состояние дискового пространства ---" >> "$LOG_FILE"
+    df -h >> "$LOG_FILE"
+    echo "--- Средняя нагрузка на CPU ---" >> "$LOG_FILE"
+    uptime >> "$LOG_FILE"
+    sleep $N
+done
