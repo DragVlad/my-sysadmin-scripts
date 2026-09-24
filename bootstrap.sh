459fd149e9a7b738c57fd9c24e3b4fa7e094015b
Владислав Драгушин <maxhc@inbox.ru>
2026-09-24 22:59:14 +0000

    Added PVS, VG, LV, FS, Mount - makers func

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,9 @@
 
 disk_1="/dev/sdb" 
 disk_2="/dev/sdc"
-raid_sleep=120
+raid_name="/dev/md0"
+vg_name="vg_storage"
+lv_name="lv_logs"
 
 install_docker() {
     if command -v docker &>/dev/null; then
@@ -33,27 +35,82 @@ EOF
 }
 
 make_raid() {
-    if command cat /proc/mdstat &>/dev/null; then
-        echo -e "\e[32mRaid massive is ready, pass\033[0m"
-        docker -v
-    fi
-}
-
-make_raid() {
     if grep -q '^md' /proc/mdstat 2>/dev/null; then
         echo -e "\e[32mRaid massive is ready, pass\033[0m"
     else
         echo -e "\e[32mStart make Raid\033[0m"
-        yes | sudo mdadm --create /dev/md0 --level=1 --raid-devices=2 $disk_1 $disk_2
+        yes | sudo mdadm --create $raid_name --level=1 --raid-devices=2 $disk_1 $disk_2
         echo -e "\e[32mWait for make Raid\033[0m"
         while grep -q 'resync' /proc/mdstat; do
             sleep 1
         done
         cat /proc/mdstat
-        echo -e "\e[32mRaid /dev/md0 Ready\033[0m"
+        echo -e "\e[32mRaid $raid_name Ready\033[0m"
         lsblk
+    fi
+}
+
+make_pvs() {
+    if sudo pvs 2>/dev/null | grep -q '/dev/md0'; then
+        echo -e "\e[32mPVS is ready, pass\033[0m"
+    else
+        sudo pvcreate $raid_name
+        sudo pvs
+        echo -e "\e[32mPVS is created\033[0m"
+    fi
+}
+
+make_vg() {
+    if sudo vgs 2>/dev/null | grep -q "$vg_name"; then
+        echo -e "\e[32mVG is ready, pass\033[0m"
+    else
+        sudo vgcreate $vg_name $raid_name
+        sudo vgs
+        echo -e "\e[32mVolume group $vg_name is created\033[0m"
+    fi
+}
+
+make_lv() {
+    if sudo lvs 2>/dev/null | grep -q "$lv_name"; then
+        echo -e "\e[32mLogical volume $lv_name is ready, pass\033[0m"
+    else
+        yes | sudo lvcreate -n $lv_name -l 100%VG $vg_name
+        sudo lvs
+        echo -e "\e[32mLogical volume $lv_name is created\033[0m"
+    fi
+}
+
+make_filesystem() {
+    if lsblk -f /dev/$vg_name/$lv_name | grep -q "ext4"; then
+        echo -e "\e[32mFileSystem ready, pass\033[0m"
+    else
+        echo -e "\e[32mFileSystem not ready\033[0m"
+        sudo mkfs.ext4 /dev/$vg_name/$lv_name
+        sudo blkid
+        echo -e "\e[32mFileSystem ready\033[0m"
+    fi
+}
+
+lv_mount() {
+    if df -h | grep "/dev/mapper/$vg_name-$lv_name"; then
+        echo -e "\e[32m/dev/$vg_name/$lv_name mounted, pass\033[0m"
+    else
+        echo -e "\e[32m/dev/$vg_name/$lv_name not mounted\033[0m"
+        sudo mount /dev/$vg_name/$lv_name /var/log
+        echo -e "\e[32m/dev/$vg_name/$lv_name mounted\033[0m"
+        df -h
+        uuid=$(sudo blkid -s UUID -o value /dev/$vg_name/$lv_name)
+        sudo tee -a /etc/fstab >/dev/null <<EOF
+UUID=${uuid}  /var/storage/smb  ext4  defaults  0 2
+EOF
+        echo -e "\e[32m$uuid added to /etc/fstab\033[0m"
     fi
 }
 
 install_docker
 make_raid
+make_pvs
+make_vg
+make_lv
+make_filesystem
+lv_mount
